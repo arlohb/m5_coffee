0f007550bf618c3ab29f46dbb9ebb06e5205450d
arlohb <arlo@urwick.co.uk>
2023-07-30 14:00:42 +0100

    Added demo

---
+++ src/Utils.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <fmt/core.h>
+#include <M5Core2.h>
+
+template<typename... T>
+static void dbg(fmt::format_string<T...> fmt, T&&... args) {
+    Serial.print(fmt::format(fmt, args...).c_str());
+};
+
+template<typename... T>
+static void dbgln(fmt::format_string<T...> fmt, T&&... args) {
+    Serial.println(fmt::format(fmt, args...).c_str());
+};
+
+#define dbgval(expr) dbgln(#expr": {}", expr)
+
+static uint16_t rgb(uint8_t r, uint8_t g, uint8_t b) {
+    // I really barely understand this format,
+    // and don't understand why they do it,
+    // but here goes:
+
+    // Its 16 bits, but without alpha
+    // 0b rrrr rggg gggb bbbb
+
+    // Reading the src code of DrawPixel,
+    // the color is sent as 16 bits,
+    // so ignore functions taking in uint32_t color
+
+    // Shrink to 5 bits
+    r >>= 3;
+    // Shrink to 6 bits
+    g >>= 2;
+    // Shrink to 5 bits
+    b >>= 3;
+
+    return (r << 11) | (g << 5) | b;
+}
+
+static uint16_t rgb(uint32_t hex) {
+    // Input here is 0x00rrggbb
+    // or 0b 0000 0000 rrrr rrrr gggg gggg bbbb bbbb
+
+    uint8_t r = (hex & 0x00FF0000) >> 16;
+    uint8_t g = (hex & 0x0000FF00) >> 8;
+    uint8_t b = hex & 0x000000FF;
+
+    return rgb(r, g, b);
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,30 @@
+#include <fmt/core.h>
+
 #include <M5Core2.h>
+
+#include "Utils.h"
 
 void setup() {
     M5.begin();
     M5.Lcd.setTextSize(3);
-    M5.Lcd.print("Hello world");
+    // From 2500 - 3300
+    M5.Axp.SetLcdVoltage(3300);
 }
 
 void loop() {
     delay(10);
     M5.update();
 
-    Serial.println(M5.BtnA.isPressed());
+    if (M5.Touch.ispressed()) {
+        Point touch = M5.Touch.getPressPoint();
+
+        M5.Lcd.drawCircle(touch.x, touch.y, 10, rgb(0x4000FF));
+    }
+
+    for (int x = 0; x < 64; x++)
+        for (int y = 0; y < 64; y++)
+            M5.Lcd.drawPixel(x, y, rgb(x * 4, 0, y * 4));
+
+    M5.Lcd.drawString(fmt::format("{}", M5.BtnA.isPressed()).c_str(), 0, 100);
 }
 
