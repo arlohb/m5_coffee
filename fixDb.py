4edaddd9cd4c9a4a99d98dcba43dd10d83825781
arlohb <arlo@urwick.co.uk>
2023-07-30 11:35:02 +0100

    Moved some unflags to fixDb.py

--- fixDb.py
+++ fixDb.py
@@ -3,6 +3,8 @@ contents = f.read()
 f.close()
 
 contents = contents.replace("-mlongcalls ", "")
+contents = contents.replace("-fstrict-volatile-bitfields ", "")
+contents = contents.replace("-fno-tree-switch-conversion ", "")
 
 f = open("compile_commands.json", "w")
 f.write(contents)
