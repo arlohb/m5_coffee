f33eb8c195615bc7a3aeda6f837fd5629055a111
arlohb <arlo@urwick.co.uk>
2025-06-16 13:56:14 +0100

    Catch errors in fixDb

--- fixDb.py
+++ fixDb.py
@@ -1,12 +1,15 @@
-f = open("compile_commands.json", "r")
-contents = f.read()
-f.close()
+try:
+    f = open("compile_commands.json", "r")
+    contents = f.read()
+    f.close()
 
-contents = contents.replace("-mlongcalls ", "")
-contents = contents.replace("-fstrict-volatile-bitfields ", "")
-contents = contents.replace("-fno-tree-switch-conversion ", "")
+    contents = contents.replace("-mlongcalls ", "")
+    contents = contents.replace("-fstrict-volatile-bitfields ", "")
+    contents = contents.replace("-fno-tree-switch-conversion ", "")
 
-f = open("compile_commands.json", "w")
-f.write(contents)
-f.close()
+    f = open("compile_commands.json", "w")
+    f.write(contents)
+    f.close()
+except Exception as e:
+    print("fixDb failed with error : ", e)
 
