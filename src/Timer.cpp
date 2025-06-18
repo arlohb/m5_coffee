3a9498dc8ea35fc6c24a90fa8cab04573fd98330
arlohb <arlo@urwick.co.uk>
2025-06-18 17:48:56 +0100

    Added custom font

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -6,9 +6,11 @@
 Timer::Timer() {
     auto root = LObject(lv_scr_act());
     btn = LButton(root);
-    btn.setLabel("00.0s");
+    btn.setLabel("00.0");
     btn.align(LV_ALIGN_CENTER, 0, 0);
     btn.setSize(120, 80);
+    
+    btn.label->align(LV_ALIGN_LEFT_MID, 8, 0);
 
     startTime = std::chrono::steady_clock::now();
 }
@@ -29,7 +31,7 @@ void Timer::update() {
     auto now = std::chrono::steady_clock::now();
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now - startTime).count();
 
-    btn.setLabel(fmt::format("{:02}.{:01}s", ms / 1000, ms % 1000 / 100));
+    btn.setLabel(fmt::format("{:02}.{:01}", ms / 1000, ms % 1000 / 100));
 }
 
 void Timer::start() {
--- src/lv_conf.h
+++ src/lv_conf.h
@@ -631,10 +631,10 @@
  *  #define LV_FONT_CUSTOM_DECLARE   LV_FONT_DECLARE(my_font_1) LV_FONT_DECLARE(my_font_2)
  *  @endcode
  */
-#define LV_FONT_CUSTOM_DECLARE
+#define LV_FONT_CUSTOM_DECLARE LV_FONT_DECLARE(lv_font_fira_code_32)
 
 /** Always set a default font */
-#define LV_FONT_DEFAULT &lv_font_montserrat_32
+#define LV_FONT_DEFAULT &lv_font_fira_code_32
 
 /** Enable handling large font and/or fonts with a lot of characters.
  *  The limit depends on the font size, font face and bpp.
