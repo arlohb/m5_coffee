04a4e356c5036ea941951a73ddf29f8ca55bcfb3
arlohb <arlo@urwick.co.uk>
2025-06-27 19:20:34 +0100

    Merged weight label and zero button on InScales

--- src/states/InScalesState.cpp
+++ src/states/InScalesState.cpp
@@ -8,22 +8,18 @@ InScalesState::InScalesState(const std::string& selectedCoffee) :
     LvglState("In Scales", true),
     selectedCoffee(selectedCoffee)
 {
-    weightLabel = lv_label_create(root);
-    lv_label_set_text(weightLabel, "00.0g");
-    lv_obj_align(weightLabel, LV_ALIGN_CENTER, -50, 0);
-    lv_obj_set_style_text_font(weightLabel, &lv_font_fira_code_32, 0);
-    
-    lv_obj_t* zero = lv_btn_create(root);
-    lv_obj_set_size(zero, 80, 40);
-    lv_obj_align(zero, LV_ALIGN_RIGHT_MID, 0, 0);
-    lv_obj_add_event_cb(zero, [] (lv_event_t* e) {
+    lv_obj_t* weightBtn = lv_btn_create(root);
+    lv_obj_set_size(weightBtn, 128, 64);
+    lv_obj_align(weightBtn, LV_ALIGN_CENTER, 0, 16);
+    lv_obj_add_event_cb(weightBtn, [] (lv_event_t* e) {
         InScalesState* state = static_cast<InScalesState*>(lv_event_get_user_data(e));
         state->scales.zero();
     }, LV_EVENT_PRESSED, this);
 
-    lv_obj_t* zeroLabel = lv_label_create(zero);
-    lv_label_set_text(zeroLabel, "Zero");
-    lv_obj_center(zeroLabel);
+    weightLabel = lv_label_create(weightBtn);
+    lv_label_set_text(weightLabel, "00.0g");
+    lv_obj_center(weightLabel);
+    lv_obj_set_style_text_font(weightLabel, &lv_font_fira_code_32, 0);
     
     lv_obj_t* nextBtn = lv_btn_create(root);
     lv_obj_set_size(nextBtn, 80, 40);
